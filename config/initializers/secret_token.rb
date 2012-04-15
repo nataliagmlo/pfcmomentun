6156a20acde6558a921704747e8460b8b866cc29
nataliagmlo <nataliagmlo@gmail.com>
2012-04-15 12:52:43 +0200

    Añadimos rails y postgre  al proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Momentun::Application.config.secret_token = 'cc3f6e654aae56b8ae3ec2b72136f32fb93dbb4dfd0490b9ba20d6d490602fcc784267b40dd466db3efedf1439fa85f78b29fc691404d4f45904aaac690dacad'
