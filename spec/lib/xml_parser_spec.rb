97072a06dbf2e4ca755e5a2f56893f9ef52539b5
nataliagmlo <nataliagmlo@gmail.com>
2012-06-28 12:30:42 +0200

    Data Collection rspec

--- spec/lib/xml_parser_spec.rb
+++ spec/lib/xml_parser_spec.rb
@@ -26,24 +26,47 @@ describe XMLParser do
     end
   end
 
-  # describe "parse_tweet_creator" do
-  #   it "parses the creator of the tweet" do
-  #     parser = Client::XmlParser.new
+  describe "parse_tweet_creator" do
+    it "parses the creator of the tweet" do
+      p = XMLParser.new
 
-  #     msg = 'user:{"name":"Sigohaciendorap","real_name":"Ana Poleon Roja","id":"262670183","language":"es","utc":"Athens + 7200","geo":null,"description":"Prefiero una libertad peligrosa a una esclavitud tranquila.","avatar":"https://si0.twimg.com/profile_images/2263582427/conestasqueesmivoz_normal.jpg","location":"","subscribers":625,"subscriptions":1274,"postings":4327,"profile":"https://twitter.com/#!/Sigohaciendorap","website":"http://loquenecesitabas.blogspot.es/"},"links":[]}';
+      msg = '{"name":"Sigohaciendorap","real_name":"Ana Poleon Roja","id":"262670183","language":"es","utc":"Athens + 7200","geo":null,"description":"Prefiero una libertad peligrosa a una esclavitud tranquila.","avatar":"https://si0.twimg.com/profile_images/2263582427/conestasqueesmivoz_normal.jpg","location":"","subscribers":625,"subscriptions":1275,"postings":4327,"profile":"https://twitter.com/#!/Sigohaciendorap","website":"http://loquenecesitabas.blogspot.es/"}';
 
 
-  #     status = JSON.parse(msg)
-  #     if status.has_key? 'Error'
-  #       raise "status malformed"
-  #     end
+      status = JSON.parse(msg)
+      if status.has_key? 'Error'
+        raise "status malformed"
+      end
 
-  #     user = parser.parse_to_tweet_creator status
+      p.parse_tweet_creator status
 
-  #     user.should_not be_nil
-  #     user.name.should_be eql, "Sigohaciendorap"
-  #   end
+      user = User.find_by_user_id "262670183"
+      user.should_not be_nil
+      user.name.should == 'Sigohaciendorap'
+      user.subscriptions.should == 1275
+      user.subscribers.should == 625
+    end
 
-  # end
+  end
+  
+  describe "parse_users_mentions" do
+    it "parses users mentions on the tweet" do
+      p = XMLParser.new
+
+      msg = '[{"name":"HoneckerRDA","id":"555993471","service":"mention","title":null,"thumbnail":null,"href":null},{"name":"PorAsturies","id":"594432676","service":"mention","title":null,"thumbnail":null,"href":null}]';
+
+
+      p.parse_tweet_creator msg
+
+      user = User.find_by_user_id '555993471'
+      user.should_not be_nil
+      user.name.should == 'HoneckerRDA'
+
+      user = User.find_by_user_id '594432676'
+      user.should_not be_nil
+      user.name.should == 'PorAsturies'
+    end
+
+  end
   #test para comprobar que aunque no haya menciones se crea el usuario en la bd creador del tweet
 end
