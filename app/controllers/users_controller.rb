edbe2dc1229fa742e16820796b033a5a629c7d55
nataliagmlo <nataliagmlo@gmail.com>
2012-08-19 15:42:50 +0200

    Change for web

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
-class UsersController < ApplicationController
+class UserController < ApplicationController
 
 	def index
-		@users = User.all
+		@user1 = User.find params["id1"]
+		@influence1 = @user1.previous_influence
+	  	@influences1 = @user1.influences 
+
+		@user2 = User.find params["id2"]
+		@influence2 = @user2.previous_influence
+	  	@influences2 = @user2.influences 
+
+
+	  	@dates = dates_for_axis(@influences1)
 	end
 
 
@@ -9,6 +18,16 @@ class UsersController < ApplicationController
 	  	@user = User.find params["id"]
 	  	@influence = @user.previous_influence
 	  	@influences = @user.influences 
+	  	@dates = dates_for_axis(@influences)
+	end
+
+	def dates_for_axis influences
+		d = ""
+		size = influences.size
+		d += "|" + influences[0].date.strftime("%d %b")
+		d += "|" + influences[size/2].date.strftime("%d %b")
+		d += "|" + influences[size-1].date.strftime("%d %b")
+		d
 	end
 
 end
